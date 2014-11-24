c013fc677c9df862dc35526ee9733550d62fdd83
Hawken Rives <hawkrives@gmail.com>
2014-11-24 09:03:10 -0600

    Update search-box

--- app/views/search.js
+++ app/views/search.js
@@ -3,8 +3,7 @@ import * as React from 'react'
 let Search = React.createClass({
 	render() {
 		return React.createElement('div', {id: 'search-screen'},
-			React.createElement('label', {htmlFor: 'f', style: {display: 'block'}}, "Search for Stuff"),
-			React.createElement('input', {id: 'f', placeholder: 'Search for ...'}))
+			React.createElement('input', {className: 'search-box', placeholder: 'Search for ...', type: 'search'}))
 	}
 })
 
