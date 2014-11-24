5a9154fadd37ee53374adc1ad40f072fc72477a4
Hawken Rives <hawkrives@gmail.com>
2014-11-24 09:02:16 -0600

    Spacing edit in header.js

--- app/components/header.js
+++ app/components/header.js
@@ -4,7 +4,8 @@ import {Link} from 'react-router'
 let Header = React.createClass({
 	render() {
 		return React.createElement("header", {id: 'header'},
-			React.createElement(Link, {to: "home"}, React.createElement("h1", null, "SVG Diplomacy")),
+			React.createElement(Link, {to: "home"},
+				React.createElement("h1", null, "SVG Diplomacy")),
 			React.createElement("ul", null,
 				React.createElement("li", null, React.createElement(Link, {to: "profile"}, "Profile")),
 				React.createElement("li", null, React.createElement(Link, {to: "search"}, "Search")),
