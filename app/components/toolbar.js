64c1c4aae7234812f72a0917e050d0c29edf5167
Hawken Rives <hawkrives@gmail.com>
2014-11-24 10:25:36 -0600

    Give each toolbar item a key based on it's name

--- app/components/toolbar.js
+++ app/components/toolbar.js
@@ -4,7 +4,7 @@ import * as _ from 'lodash'
 let Toolbar = React.createClass({
 	render() {
 		return React.createElement('ul', {className: 'toolbar'},
-			_.map(this.props.tools, tool => React.createElement('li', {className: 'toolbar-item'}, tool)))
+			_.map(this.props.tools, tool => React.createElement('li', {className: 'toolbar-item', key: tool}, tool)))
 	}
 })
 
