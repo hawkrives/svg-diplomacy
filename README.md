66ee295971e3b4bf4630816b78342324d87d33e1
Hawken Rives <hawkrives@gmail.com>
2014-11-23 19:26:28 -0600

    Finish build system skeleton

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 svg-diplomacy
 =============
+
+To set up:
+
+- Clone
+- `npm install -g gulp`
+- `gulp build`
---
+++ gulp/config.js
@@ -0,0 +1,42 @@
+var dest = "./build/";
+var src = './app/';
+
+module.exports = {
+	browserSync: {
+		server: {
+			// We're serving the src folder as well
+			// for sass sourcemap linking
+			baseDir: [dest, src]
+		},
+		files: [
+			dest + "**",
+			// Exclude Map files
+			"!" + dest + "**.map"
+		]
+	},
+	sass: {
+		src: src + "sass/*.{sass,scss}",
+		dest: dest
+	},
+	images: {
+		src: src + "images/**",
+		dest: dest + "images"
+	},
+	markup: {
+		src: src + "htdocs/**",
+		dest: dest
+	},
+	browserify: {
+		// Enable source maps
+		debug: true,
+		// Additional file extentions to make optional
+		// extensions: ['.coffee', '.hbs'],
+		// A separate bundle will be generated for each
+		// bundle config in the list below
+		bundleConfigs: [{
+			entries: src + 'index.js',
+			dest: dest,
+			outputName: 'app.js'
+		}]
+	}
+};
