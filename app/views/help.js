ba0d9f8b7f661dc765ccee3f23f623f36375a691
Hawken Rives <hawkrives@gmail.com>
2014-11-29 14:52:17 -0600

    Initial pass on Help view

--- app/views/help.js
+++ app/views/help.js
@@ -1,8 +1,23 @@
 import * as React from 'react'
+import {Link} from 'react-router'
 
 let Help = React.createClass({
 	render() {
-		return React.createElement('div', {id: 'help'}, "Help Screen")
+		let rules = React.createElement('article', {className: 'short-rules'},
+			React.createElement(Link, {to: 'rules'}, React.createElement('h2', {className: 'subtitle'}, 'Rules')),
+			React.createElement('p', null, "Rules! Rules rules rules rules, rule rules rules rules."))
+
+		let tutorial = React.createElement('article', {className: 'short-tutorial'},
+			React.createElement(Link, {to: 'tutorial'}, React.createElement('h2', {className: 'subtitle'}, 'Tutorial')),
+			React.createElement('p', null, "Tutorial! Tutorial tutorial tutorial tutorial, rule tutorial tutorial tutorial."))
+
+		let title = React.createElement('h1', {className: 'view-title'}, 'Help')
+
+		return React.createElement('div',
+			{id: 'help'},
+			title,
+			rules,
+			tutorial)
 	}
 })
 
