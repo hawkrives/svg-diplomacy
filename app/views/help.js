335ad38a485c9c289e47aeec2c6f453371a38371
Hawken Rives <hawkrives@gmail.com>
2014-11-29 18:26:04 -0600

    Fetch and render the markdown files into the help screen

--- app/views/help.js
+++ app/views/help.js
@@ -1,15 +1,37 @@
 import * as React from 'react'
 import {Link} from 'react-router'
+import * as marked from 'marked'
+import 'fetch'
 
 let Help = React.createClass({
+	getInitialState() {
+		return {
+			rulesHTML: '<p>Loading Rules&hellip;</p>',
+			tutorialHTML: '<p>Loading Tutorial&hellip;</p>'
+		}
+	},
+	componentWillMount() {
+		let pathToRules = 'rules.text';
+		let pathToTutorial = 'tutorial.text';
+
+		fetch(pathToRules)
+			.then((response) => {
+				this.setState({rulesHTML: marked(response._body)})
+			})
+		
+		fetch(pathToTutorial)
+			.then((response) => {
+				this.setState({tutorialHTML: marked(response._body)})
+			})
+	},
 	render() {
 		let rules = React.createElement('article', {className: 'short-rules'},
 			React.createElement(Link, {to: 'rules'}, React.createElement('h2', {className: 'subtitle'}, 'Rules')),
-			React.createElement('p', null, "Rules! Rules rules rules rules, rule rules rules rules."))
+			React.createElement('div', {dangerouslySetInnerHTML: {__html: this.state.rulesHTML}}))
 
 		let tutorial = React.createElement('article', {className: 'short-tutorial'},
 			React.createElement(Link, {to: 'tutorial'}, React.createElement('h2', {className: 'subtitle'}, 'Tutorial')),
-			React.createElement('p', null, "Tutorial! Tutorial tutorial tutorial tutorial, rule tutorial tutorial tutorial."))
+			React.createElement('div', {dangerouslySetInnerHTML: {__html: this.state.tutorialHTML}}))
 
 		let title = React.createElement('h1', {className: 'view-title'}, 'Help')
 
