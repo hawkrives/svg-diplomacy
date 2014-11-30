38ad3ffd444ce8c4d2dbefd8caae58bc3598098a
Hawken Rives <hawkrives@gmail.com>
2014-11-30 09:11:34 -0600

    Update game-list appearance

--- app/components/game-list.js
+++ app/components/game-list.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import * as _ from 'lodash'
 import {Link} from 'react-router'
 import Subtitle from './subtitle'
+import Octicon from './octicon'
 
 let GameList = React.createClass({
 	propTypes: {
@@ -12,16 +13,18 @@ let GameList = React.createClass({
 	render() {
 		let title = React.createElement(Subtitle, {text: this.props.title});
 
-		let games = this.props.games
-
+		let listOfGames = React.createElement('ul', null, _.map(this.props.games, (game) =>
 			React.createElement('li', {key: game.id},
-				React.createElement(Link, {to: 'game', params: {gameId: game.id}}, game.title)))
+				React.createElement(Link, {to: 'game', params: {gameId: game.id}},
+					React.createElement('span', {className: 'game-title'}, game.title),
+					React.createElement('span', {className: 'map-name'}, game.map.name),
+					React.createElement(Octicon, {icon: 'chevron-right'})))))
 
 		let noGamesMessage = "No games available."
 
 		return React.createElement('section', {className: 'game-list'},
 			title,
-			React.createElement('ul', null, _.isEmpty(listOfGames) ? noGamesMessage : listOfGames ))
+			_.isEmpty(listOfGames) ? noGamesMessage : listOfGames)
 	}
 })
 
