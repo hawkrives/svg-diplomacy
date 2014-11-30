f84a8a7f8bf16849e86442c371eaa3db61ee0506
Hawken Rives <hawkrives@gmail.com>
2014-11-30 09:10:53 -0600

    Remove ability to filter within the game-list component

--- app/components/game-list.js
+++ app/components/game-list.js
@@ -7,8 +7,6 @@ let GameList = React.createClass({
 	propTypes: {
 		games: React.PropTypes.array.isRequired,
 		title: React.PropTypes.string.isRequired,
-		filter: React.PropTypes.object,
-		reject: React.PropTypes.object,
 	},
 
 	render() {
@@ -16,14 +14,6 @@ let GameList = React.createClass({
 
 		let games = this.props.games
 
-		if (this.props.filter || this.props.reject) {
-			games = _.chain(this.props.games)
-				.filter(this.props.filter)
-				.reject(this.props.reject || {})
-				.value()
-		}
-
-		let listOfGames = _.map(games, (game) =>
 			React.createElement('li', {key: game.id},
 				React.createElement(Link, {to: 'game', params: {gameId: game.id}}, game.title)))
 
--- app/views/home.js
+++ app/views/home.js
@@ -1,23 +1,46 @@
 import * as React from 'react'
+import * as _ from 'lodash'
 import {currentUser} from '../helpers/auth'
 import GameList from '../components/game-list'
 
 let gameObjects = [
 	{
+		id: 'kfj309fj20j0asdadas',
+		owner: '123j33dcj0',
+		players: ['983nf01laa', '123j33dcj0'],
+		map: {name: 'Northfield', id: '91dj2091'},
+		title: 'Diplomacy is Dead',
+		moves: [
+			{user: '8WswhCVIse', from: 'St. Olaf', to: 'Naturalands'},
+			{user: '123j33dcj0', from: 'Carleton', to: 'The Arb'},
+		],
+	},
+	{
 		id: 'sdywf9874ia8hos9290',
 		owner: '8WswhCVIse',
-		players: ['hawkrives', 'heroine', 'dominator'],
-		mapName: 'Europe',
-		mapId: '91nf8932',
+		players: ['8WswhCVIse', '983nf01laa', '123j33dcj0'],
+		map: {name: 'Europe', id: '91nf8932'},
 		title: 'STOLAF DOMINATES EUROPE!',
-		moves: [{user: '91nf8932', from: 'Paris', to: 'Burgandy'}],
-	}
+		moves: [{user: '8WswhCVIse', from: 'Paris', to: 'Burgandy'}],
+	},
+	{
+		id: '892bw2f39h08ehf2332',
+		owner: '123j33dcj0',
+		players: ['8WswhCVIse', '123j33dcj0'],
+		map: {name: 'Northfield', id: '91dj2091'},
+		title: 'CARLETON vs STOLAF!',
+		moves: [
+			{user: '8WswhCVIse', from: 'St. Olaf', to: 'Naturalands'},
+			{user: '123j33dcj0', from: 'Carleton', to: 'The Arb'},
+		],
+	},
 ]
 
 let Home = React.createClass({
 	render() {
+		let myGameObjects = _.filter(gameObjects, (game) => _.contains(game.players, currentUser().id))
 		let allGames = React.createElement(GameList, {title: 'All Games', games: gameObjects})
-		let myGames = React.createElement(GameList, {title: 'My Games', games: gameObjects, filter: {owner: currentUser().id}})
+		let myGames = React.createElement(GameList, {title: 'My Games', games: myGameObjects})
 
 		return React.createElement('div', {id: 'home'},
 			myGames,
