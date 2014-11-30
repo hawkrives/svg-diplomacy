ea31b225e46e2c8419082425a6c0644ca6355718
Hawken Rives <hawkrives@gmail.com>
2014-11-29 18:39:18 -0600

    Create a Subtitle component
    for making subtitles on pages

---
+++ app/components/subtitle.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+let Subtitle = React.createClass({
+	propTypes: {
+		text: React.PropTypes.string.isRequired,
+	},
+	render() {
+		return React.createElement('h2', {className: 'subtitle'}, this.props.text)
+	}
+})
+
+export default Subtitle
