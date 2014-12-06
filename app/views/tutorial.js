785fe09afb8f50492f17e5ebc615cd3b8f7aff59
knightflight377 <emholm12@gmail.com>
2014-12-06 12:57:41 -0600

    Created tutorial page

--- app/views/tutorial.js
+++ app/views/tutorial.js
@@ -1,13 +1,33 @@
 import * as React from 'react'
+import * as marked from 'marked'
 
 let Tutorial = React.createClass({
+	getInitialState() {
+		return {
+			tutorialHTML: '<p>Loading Tutorial&hellip;</p>',
+		}
+	},
+	componentWillMount() {
+		let pathToTutorial = 'detailed-tutorial.text';
+
+		fetch(pathToTutorial)
+			.then((response) => {
+				this.setState({tutorialHTML: marked(response._body)})
+			})
+	},
 	render() {
+		let tutorial = React.createElement('article', {className: 'detailed-tutorial'},
+			React.createElement('div', {dangerouslySetInnerHTML: {__html: this.state.tutorialHTML}}))
+
 		let title = React.createElement('h1', {className: 'view-title'}, 'Tutorial')
 
 		return React.createElement('div',
 			{id: 'tutorial'},
-			title)
+			title,
+			tutorial)
 	}
 })
 
 export default Tutorial
+
+
