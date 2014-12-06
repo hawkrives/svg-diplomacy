5709beb57a9ece646250fe394299014e4725b9cd
knightflight377 <emholm12@gmail.com>
2014-12-06 12:57:53 -0600

    Created rules page

--- app/views/rules.js
+++ app/views/rules.js
@@ -1,13 +1,32 @@
 import * as React from 'react'
+import * as marked from 'marked'
 
 let Rules = React.createClass({
+	getInitialState() {
+		return {
+			rulesHTML: '<p>Loading Rules&hellip;</p>',
+		}
+	},
+	componentWillMount() {
+		let pathToRules = 'detailed-rules.text';
+
+		fetch(pathToRules)
+			.then((response) => {
+				this.setState({rulesHTML: marked(response._body)})
+			})
+	},
 	render() {
+		let rules = React.createElement('article', {className: 'detailed-rules'},
+			React.createElement('div', {dangerouslySetInnerHTML: {__html: this.state.rulesHTML}}))
+
 		let title = React.createElement('h1', {className: 'view-title'}, 'Rules')
 
 		return React.createElement('div',
 			{id: 'rules'},
-			title)
+			title,
+			rules)
 	}
 })
 
 export default Rules
+
