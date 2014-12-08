7d14b0e88bb8d12a1abb06da849b3a8f4a66eeff
Hawken Rives <hawkrives@gmail.com>
2014-12-08 00:52:31 -0600

    Create initial listing and creation of maps

--- app/views/create.js
+++ app/views/create.js
@@ -3,6 +3,23 @@ import * as Reflux from 'reflux'
 import * as _ from 'lodash'
 import Toolbar from '../components/toolbar'
 import mapStore from '../stores/mapStore'
+import mapActions from '../actions/mapActions'
+
+let MapListItem = React.createClass({
+	destroyMap(ev) {
+		ev.preventDefault()
+		mapActions.destroyMap(this.props.map.id)
+	},
+	render() {
+		let components = _.map(this.props.map.attributes, (value, key) => {
+			return React.createElement('div', {className: `map-part-${key}`}, key, ': ', value)
+		})
+		components.unshift(React.createElement('div', {className: 'map-part-id'}, 'id: ', this.props.map.id))
+		components.push(React.createElement('button', {onClick: this.destroyMap}, 'Delete Map'))
+		return React.createElement('div', null,
+			components)
+	},
+})
 
 let Create = React.createClass({
 	mixins: [Reflux.listenTo(mapStore, 'onMapsChanged', 'onMapsChanged')],
@@ -14,16 +31,37 @@ let Create = React.createClass({
 	onMapsChanged(newMaps) {
 		this.setState({maps: newMaps})
 	},
+	createMap(ev) {
+		ev.preventDefault()
+		mapActions.createMap({
+			name: this.refs.name.getDOMNode().value,
+			players: this.refs.players.getDOMNode().value,
+			width: this.refs.width.getDOMNode().value,
+			height: this.refs.height.getDOMNode().value,
+			countries: this.refs.countries.getDOMNode().value,
+			spaces: this.refs.spaces.getDOMNode().value,
+		})
+	},
 	render() {
 		let title = React.createElement('h1', {className: 'view-title'}, 'Create New ...')
 
-		let listOfMaps = React.createElement('ul', null, _.map(this.state.maps, map =>
-			React.createElement('li', null, JSON.stringify(map))))
+		let listOfMaps = React.createElement('ul', {className: 'raw-map-list'}, _.map(this.state.maps, map =>
+			React.createElement('li', {key: map.id}, React.createElement(MapListItem, {map: map}))))
+
+		let mapCreationForm = React.createElement('form', {className: 'map-creation-form', onSubmit: this.createMap},
+			React.createElement('label', null, 'Name ', React.createElement('input', {type: 'text', ref: 'name', placeholder: 'Map Name'})),
+			React.createElement('label', null, 'Width ', React.createElement('input', {type: 'text', ref: 'width', placeholder: 'Map Width (in pixels)'})),
+			React.createElement('label', null, 'Height ', React.createElement('input', {type: 'text', ref: 'height', placeholder: 'Map Height (in pixels)'})),
+			React.createElement('label', null, 'Number of Players ', React.createElement('input', {type: 'number', ref: 'players', placeholder: '2'})),
+			React.createElement('label', null, 'Countries ', React.createElement('input', {type: 'text', ref: 'countries', placeholder: '[{/*CountryObjects*/}]'})),
+			React.createElement('label', null, 'Spaces ', React.createElement('input', {type: 'text', ref: 'spaces', placeholder: '[{/*SpaceObjects*/}]'})),
+			React.createElement('input', {type: 'submit', value: 'Create new map'}))
 
 		return React.createElement('div',
 			{id: 'create'},
 			title,
 			listOfMaps,
+			mapCreationForm,
 			React.createElement(Toolbar, {tools: ['Create new Game', 'Create new Map']}))
 	},
 })
