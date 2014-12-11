e8f7fa66e90b654e9f7c641f591094e43906fa85
Hawken Rives <hawkrives@gmail.com>
2014-12-10 23:12:26 -0600

    Handle errors in the linting task

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var handleErrors = require('../util/handleErrors');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 var stylish = require('jshint-stylish');
@@ -7,6 +8,8 @@ var config = require('../config').lint;
 gulp.task('lint', function() {
 	return gulp.src(config.src)
 		.pipe(jscs())
+		.on('error', handleErrors)
 		.pipe(jshint('.jshintrc'))
+		.on('error', handleErrors)
 		.pipe(jshint.reporter('jshint-stylish'))
 });
