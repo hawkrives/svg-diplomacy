f7b10d6c0b1a7d2861c0afa599616526ffaff9f4
Hawken Rives <hawkrives@gmail.com>
2014-12-11 09:47:45 -0600

    Double-quotes -> single-quotes

--- app/views/map.js
+++ app/views/map.js
@@ -143,14 +143,14 @@ let RenderedMap = React.createClass({
 				)
 			)
 
-		console.log("sea", seaSpaces)
+		console.log('sea', seaSpaces)
 
 		let emptySpaces = _(this.state.map.spaces)
 			.reject((space) => _.contains(occupiedSpaces, space.id))
 			.reject((space) => _.contains(seaSpaceIds, space.id))
 			.value()
 
-		console.log("empty", emptySpaces)
+		console.log('empty', emptySpaces)
 
 		let otherSpaces = React.createElement('g',
 			{
