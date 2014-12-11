6ef218500596f32b25062f993d898039b22e88d9
Hawken Rives <hawkrives@gmail.com>
2014-12-11 16:05:13 -0600

    Spice up the map creation form a bit

--- app/views/create.js
+++ app/views/create.js
@@ -10,12 +10,15 @@ let MapListItem = React.createClass({
 		mapActions.destroyMap(this.props.map.id)
 	},
 	render() {
-		let components = _.map(this.props.map.attributes, (value, key) => {
-			return React.createElement('div', {className: `map-part-${key}`, key: key},
-				React.createElement('span', {className: 'key'}, key, ': '), JSON.stringify(value, null, 2))
+		let components = _.map(this.props.map.attributes || this.props.map, (value, key) => {
+			let val = JSON.stringify(value, null, 2);
+			if (key === 'id' || key === 'get')
+				return null
+			return React.createElement('div', {key: key},
+				 key, ': ', val ? val.substring(0, 50) : val)
 		})
-		components.unshift(React.createElement('div', {className: 'map-part-id', key: 'id'},
-			React.createElement('span', {className: 'key'}, 'id: '), this.props.map.id))
+		components.unshift(React.createElement('div', {key: 'id'},
+			'id', ': ', this.props.map.id))
 		components.push(React.createElement('button', {onClick: this.destroyMap, key: 'deleteButton'}, 'Delete Map'))
 		return React.createElement('div', {className: 'one-map'},
 			components)
@@ -37,16 +40,19 @@ let Create = React.createClass({
 	render() {
 		let title = React.createElement('h1', {className: 'view-title'}, 'Create New ...')
 
-		let listOfMaps = React.createElement('ul', {className: 'raw-map-list'}, _.map(this.props.maps, map =>
-			React.createElement('li', {key: map.id}, React.createElement(MapListItem, {map: map}))))
+		let listOfMaps = React.createElement('ul',
+			{className: 'raw-map-list'},
+			_.map(this.props.maps, (map) =>
+				React.createElement('li', {key: map.id},
+					React.createElement(MapListItem, {map: map}))))
 
 		let mapCreationForm = React.createElement('form', {className: 'map-creation-form', onSubmit: this.createMap},
-			React.createElement('label', null, 'Name ', React.createElement('input', {type: 'text', ref: 'name', placeholder: 'Map Name'})),
-			React.createElement('label', null, 'Width ', React.createElement('input', {type: 'text', ref: 'width', placeholder: 'Map Width (in pixels)'})),
-			React.createElement('label', null, 'Height ', React.createElement('input', {type: 'text', ref: 'height', placeholder: 'Map Height (in pixels)'})),
-			React.createElement('label', null, 'Number of Players ', React.createElement('input', {type: 'number', ref: 'players', placeholder: '2'})),
-			React.createElement('label', null, 'Countries ', React.createElement('input', {type: 'text', ref: 'countries', placeholder: '[{/*CountryObjects*/}]'})),
-			React.createElement('label', null, 'Spaces ', React.createElement('input', {type: 'text', ref: 'spaces', placeholder: '[{/*SpaceObjects*/}]'})),
+			React.createElement('label', null, 'Name: ', React.createElement('input', {type: 'text', ref: 'name', placeholder: 'Map Name'})),
+			React.createElement('label', null, 'Width: ', React.createElement('input', {type: 'text', ref: 'width', placeholder: 'Map Width (in pixels)'})),
+			React.createElement('label', null, 'Height: ', React.createElement('input', {type: 'text', ref: 'height', placeholder: 'Map Height (in pixels)'})),
+			React.createElement('label', null, 'Number of Players: ', React.createElement('input', {type: 'number', ref: 'players', placeholder: '2'})),
+			React.createElement('label', null, 'Countries: ', React.createElement('input', {type: 'text', ref: 'countries', placeholder: '[{/*CountryObjects*/}]'})),
+			React.createElement('label', null, 'Spaces: ', React.createElement('input', {type: 'text', ref: 'spaces', placeholder: '[{/*SpaceObjects*/}]'})),
 			React.createElement('input', {type: 'submit', value: 'Create new map'}))
 
 		return React.createElement('div',
