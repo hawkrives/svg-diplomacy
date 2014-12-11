259b613c68d7309aa6a03c9cecb8ff13a87f08e6
Hawken Rives <hawkrives@gmail.com>
2014-12-10 23:35:35 -0600

    Fetch app state in App and pass down via props

--- app/views/app.js
+++ app/views/app.js
@@ -1,14 +1,44 @@
 import * as React from 'react'
+import * as Reflux from 'reflux'
 import {RouteHandler} from 'react-router'
+
+import userStore from '../stores/userStore'
+import gameStore from '../stores/gameStore'
+import mapStore from '../stores/mapStore'
 
 import Header from '../components/header'
 
 let App = React.createClass({
+	mixins: [
+		Reflux.listenTo(userStore, 'onUserChanged', 'onUserChanged'),
+		Reflux.listenTo(gameStore, 'onGameChanged', 'onGameChanged'),
+		Reflux.listenTo(mapStore,  'onMapChanged',  'onMapChanged'),
+	],
+	onUserChanged(user) {
+		console.log('app.onUserChanged')
+		this.setState({user})
+	},
+	onGameChanged(games) {
+		console.log('app.onGameChanged')
+		this.setState({games})
+	},
+	onMapChanged(maps) {
+		console.log('app.onMapChanged')
+		this.setState({maps})
+	},
+	getInitialState() {
+		return {
+			user: null,
+			maps: [],
+			games: [],
+		}
+	},
 	render() {
 		return React.createElement('div', {className: 'app'},
 			React.createElement(Header, null),
 			React.createElement('main', null,
-				React.createElement(RouteHandler, null)))
+				React.createElement(RouteHandler,
+					{maps: this.state.maps, user: this.state.user, games: this.state.games})))
 	},
 })
 
