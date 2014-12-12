0115e1501663fb7e4258e92bb4d3f8d827484c8a
Hawken Rives <hawkrives@gmail.com>
2014-12-12 00:09:58 -0600

    Pass the maps array to GameList

--- app/views/home.js
+++ app/views/home.js
@@ -19,17 +19,15 @@ let Home = React.createClass({
 		}
 	},
 	render() {
-		console.log('homeProps', this.props)
-
 		if (this.state.loading)
 			return React.createElement('div', {id: 'home'}, 'Loading...')
 
-		let allGames = React.createElement(GameList, {title: 'All Games', games: this.props.games})
+		let allGames = React.createElement(GameList, {title: 'All Games', games: this.props.games, maps: this.props.maps})
 
 		let myGames;
 		if (this.props.user) {
 			let myGameObjects = _.filter(this.props.games, (game) => _.contains(game.get('players'), this.props.user.id))
-			myGames = React.createElement(GameList, {title: 'My Games', games: myGameObjects})
+			myGames = React.createElement(GameList, {title: 'My Games', games: myGameObjects, maps: this.props.maps})
 		}
 
 		return React.createElement('div', {id: 'home'},
