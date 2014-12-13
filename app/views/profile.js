c9808bafa21c51472419547f2a35cc78aef29165
Hawken Rives <hawkrives@gmail.com>
2014-12-13 12:03:10 -0600

    Update the profile styling

--- app/views/profile.js
+++ app/views/profile.js
@@ -20,15 +20,9 @@ let Profile = React.createClass({
 			saving: false,
 		}
 	},
-	onNameChange(ev) {
-		this.setState({name: ev.target.value})
-	},
-	onEmailChange(ev) {
-		this.setState({email: ev.target.value})
-	},
-	onUsernameChange(ev) {
-		this.setState({username: ev.target.value})
-	},
+	onNameChange(ev) { this.setState({name: ev.target.value}) },
+	onEmailChange(ev) { this.setState({email: ev.target.value}) },
+	onUsernameChange(ev) { this.setState({username: ev.target.value}) },
 	saveComplete() {
 		this.setState({saving: 'saved'})
 		console.log('saved user')
@@ -47,29 +41,29 @@ let Profile = React.createClass({
 		let title = React.createElement('h1', {className: 'view-title'}, 'User\'s Profile')
 
 		let fields = [
-			React.createElement('label', {key: 'name'}, 'Name: ', React.createElement('input',
-				{type: 'text', placeholder: 'Anne Onymous', value: this.state.name, onChange: this.onNameChange})),
+			React.createElement('label', {key: 'name-label', htmlFor: 'name'}, 'Name: '),
+			React.createElement('input', {key: 'name-input', id: 'name', type: 'text', placeholder: 'Anne Onymous', value: this.state.name, onChange: this.onNameChange}),
 
-			React.createElement('label', {key: 'email'}, 'Email: ', React.createElement('input',
-				{type: 'email', placeholder: 'anne@onymo.us', value: this.state.email, onChange: this.onEmailChange})),
+			React.createElement('label', {key: 'email-label', htmlFor: 'email'}, 'Email: '),
+			React.createElement('input', {key: 'email-input', id: 'email', type: 'email', placeholder: 'anne@onymo.us', value: this.state.email, onChange: this.onEmailChange}),
 
-			React.createElement('label', {key: 'username'}, 'Username: ', React.createElement('input',
-				{type: 'username', placeholder: 'anne', value: this.state.username, onChange: this.onUsernameChange})),
+			React.createElement('label', {key: 'username-label', htmlFor: 'username'}, 'Username: '),
+			React.createElement('input', {key: 'username-input', id: 'username', type: 'text', placeholder: 'anne', value: this.state.username, onChange: this.onUsernameChange}),
 
-			React.createElement('label', {key: 'password'}, 'Password: ', React.createElement('input',
-				{type: 'password', placeholder: '·······', defaultValue: '', ref: 'password'})),
+			React.createElement('label', {key: 'password-label', htmlFor: 'password'}, 'Password: '),
+			React.createElement('input', {key: 'password-input', id: 'password', type: 'password', placeholder: '·······', defaultValue: '', ref: 'password'}),
 		]
 
 		let buttons = React.createElement('div', {className: 'buttons'},
-			React.createElement('input', {type: 'submit', value: 'Save'}),
-			React.createElement('button', {onClick: userActions.willSignOut}, 'Sign Out'))
+			React.createElement('input', {className: 'save-profile', type: 'submit', value: 'Save'}),
+			React.createElement('button', {className: 'sign-out', onClick: userActions.willSignOut}, 'Sign Out'))
 
-		let profile = JSON.stringify(this.props.user, null, 2);
+		// let profile = JSON.stringify(this.props.user, null, 2);
 
 		return React.createElement('div',
 			{id: 'profile'},
 			title,
-			profile,
+			// profile,
 			React.createElement('form', {className: 'edit-profile', onSubmit: this.update},
 				fields,
 				buttons))
