c79d4fee907c01e8a1acb8d33fbac5be67493f9f
Hawken Rives <hawkrives@gmail.com>
2014-12-14 16:22:18 -0600

    Use the RouterState mixin in GameNav

--- app/components/game-nav.js
+++ app/components/game-nav.js
@@ -1,23 +1,26 @@
 import * as React from 'react'
+import {State} from 'react-router'
 import {Octicon as OcticonClass} from './octicon'
-import {Link} from 'react-router'
+import {Link as LinkClass} from 'react-router'
 
+let Link = React.createFactory(LinkClass);
 let Octicon = React.createFactory(OcticonClass);
 
 let GameNavbar = React.createClass({
+	mixins: [State],
 	render() {
 		return React.createElement('ul', {id: 'game-nav', className: 'menu'},
-			React.createElement('li', {key: 'board'},
-				React.createElement(Link, {to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'board'}}, Octicon({icon: 'globe'}
+			React.createElement('li', null,
+				Link({to: 'game', params: this.getParams(), query: {section: 'board'}}, Octicon({icon: 'globe'}
 			))),
-			React.createElement('li', {key: 'chat'},
-				React.createElement(Link, {to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'chat'}}, Octicon({icon: 'comment-discussion'}
+			React.createElement('li', null,
+				Link({to: 'game', params: this.getParams(), query: {section: 'chat'}}, Octicon({icon: 'comment-discussion'}
 			))),
-			React.createElement('li', {key: 'history'},
-				React.createElement(Link, {to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'history'}}, Octicon({icon: 'clock'}
+			React.createElement('li', null,
+				Link({to: 'game', params: this.getParams(), query: {section: 'history'}}, Octicon({icon: 'clock'}
 			))),
-			React.createElement('li', {key: 'info'},
-				React.createElement(Link, {to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'info'}}, Octicon({icon: 'gear'}
+			React.createElement('li', null,
+				Link({to: 'game', params: this.getParams(), query: {section: 'info'}}, Octicon({icon: 'gear'}
 			)))
 		)
 	},
