2b944ed159d5f9087e8ac0ee5f16dd22a3dca6fc
Nick Nooney <nooney@stolaf.edu>
2014-12-13 23:27:09 -0600

    Changed toolbar formatting to actually create links, and styled them similar to game-list

--- app/components/toolbar.js
+++ app/components/toolbar.js
@@ -1,10 +1,13 @@
 import * as React from 'react'
 import * as _ from 'lodash'
+import {Link} from 'react-router'
 
 let Toolbar = React.createClass({
 	render() {
-		return React.createElement('ul', {className: 'toolbar'},
-			_.map(this.props.tools, tool => React.createElement('li', {className: 'toolbar-item', key: tool}, tool)))
+		return React.createElement('ul', {className: 'toolbar'}, _.map(this.props.tools, (tool) =>
+			React.createElement('li', {className: 'toolbar-item', key: tool},
+				React.createElement(Link, {to: tool, className: 'tool'}, tool
+			))))
 	}
 })
 
