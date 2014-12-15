cb3ee60647cbb5325020bd802b45c76f05b7d9ea
Hawken Rives <hawkrives@gmail.com>
2014-12-14 23:15:48 -0600

    Clean up some spacing in game-nav; switch to passing all params to the links

--- app/components/game-nav.js
+++ app/components/game-nav.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-
+import {State} from 'react-router'
 import {Octicon as OcticonClass} from './octicon'
 import {Link as LinkClass} from 'react-router'
 
@@ -7,11 +7,12 @@ let Octicon = React.createFactory(OcticonClass)
 let Link = React.createFactory(LinkClass)
 
 let GameNavbar = React.createClass({
+	mixins: [State],
 	render() {
-		let boardLink	= React.createElement('li', {key: 'board'},		Link({to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'board'}},	Octicon({icon: 'globe'})))
-		let chatLink	= React.createElement('li', {key: 'chat'},		Link({to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'chat'}},	Octicon({icon: 'comment-discussion'})))
-		let historyLink	= React.createElement('li', {key: 'history'},	Link({to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'history'}},	Octicon({icon: 'clock'})))
-		let infoLink	= React.createElement('li', {key: 'info'},		Link({to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'info'}},	Octicon({icon: 'gear'})))
+		let boardLink   = React.createElement('li', {key: 'board'},   Link({to: 'game', params: this.getParams(), query: {section: 'board'}},   Octicon({icon: 'globe'})))
+		let chatLink    = React.createElement('li', {key: 'chat'},    Link({to: 'game', params: this.getParams(), query: {section: 'chat'}},    Octicon({icon: 'comment-discussion'})))
+		let historyLink = React.createElement('li', {key: 'history'}, Link({to: 'game', params: this.getParams(), query: {section: 'history'}}, Octicon({icon: 'clock'})))
+		let infoLink    = React.createElement('li', {key: 'info'},    Link({to: 'game', params: this.getParams(), query: {section: 'info'}},    Octicon({icon: 'gear'})))
 
 		let navItems = []
 
