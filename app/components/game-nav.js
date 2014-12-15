8b99d21d350a4dc7865e0b66cc293a95b0fa5bb5
Nick Nooney <nooney@stolaf.edu>
2014-12-14 18:16:16 -0600

    Changed rendering of navbar based upon game state, similar to auth, which is based on user state

--- app/components/game-nav.js
+++ app/components/game-nav.js
@@ -1,25 +1,33 @@
 import * as React from 'react'
-import {Octicon as OcticonClass} from './octicon'
-import {Link} from 'react-router'
 
-let Octicon = React.createFactory(OcticonClass);
+import {Octicon as OcticonClass} from './octicon'
+import {Link as LinkClass} from 'react-router'
+
+let Octicon = React.createFactory(OcticonClass)
+let Link = React.createFactory(LinkClass)
 
 let GameNavbar = React.createClass({
 	render() {
-		return React.createElement('ul', {id: 'game-nav', className: 'menu'},
-			React.createElement('li', {key: 'board'},
-				React.createElement(Link, {to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'board'}}, Octicon({icon: 'globe'}
-			))),
-			React.createElement('li', {key: 'chat'},
-				React.createElement(Link, {to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'chat'}}, Octicon({icon: 'comment-discussion'}
-			))),
-			React.createElement('li', {key: 'history'},
-				React.createElement(Link, {to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'history'}}, Octicon({icon: 'clock'}
-			))),
-			React.createElement('li', {key: 'info'},
-				React.createElement(Link, {to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'info'}}, Octicon({icon: 'gear'}
-			)))
-		)
+		let preGameItems = [
+			React.createElement('li', {key: 'board'},	Link({to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'board'}},	Octicon({icon: 'globe'}))),
+			React.createElement('li', {key: 'info'},	Link({to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'info'}},	Octicon({icon: 'gear'}))),
+		]
+
+		let activeGameItems = [
+			React.createElement('li', {key: 'board'},	Link({to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'board'}},	Octicon({icon: 'globe'}))),
+			React.createElement('li', {key: 'chat'},	Link({to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'chat'}},	Octicon({icon: 'comment-discussion'}))),
+			React.createElement('li', {key: 'history'},	Link({to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'history'}},	Octicon({icon: 'clock'}))),
+			React.createElement('li', {key: 'info'},	Link({to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'info'}},	Octicon({icon: 'gear'}))),
+		]
+
+		let completedGameItems = [
+			React.createElement('li', {key: 'history'},	Link({to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'history'}},	Octicon({icon: 'clock'}))),
+			React.createElement('li', {key: 'info'},	Link({to: 'game', params: {gameId: this.props.params.gameId}, query: {section: 'info'}},	Octicon({icon: 'gear'}))),
+		]
+
+		let navItems = activeGameItems
+
+		return React.createElement('ul', {id: 'game-nav', className: 'menu'}, navItems)
 	},
 })
 
