756aa4964f5cccf2cc8b511a11dedca45571f2b9
Nick Nooney <nooney@stolaf.edu>
2014-12-16 12:49:56 -0600

    Added more game logic to the backend

--- backend/cloud/main.js
+++ backend/cloud/main.js
@@ -1,6 +1,29 @@
 Parse.Cloud.beforeSave('Game', function(request, response) {
-	if(request.object.get('status') === undefined) {
+	if (request.object.get('status') === undefined) {
 		request.object.set('status', 'preGame');
+		response.success();
+		return;
 	}
+	else if (request.object.get('status') === 'preGame') {
+		var query = new Parse.Query("Map");
+
+		query.get(request.object.id, {
+			success: function(result) {
+				if(request.object.get('players').length >= result.get('players'))
+					resquest.object.set('status', 'active')
+
+				response.success();
+				return;
+			},
+			error: function(error) {
+				response.error("Query failed. Error: " + error.message);
+				return;
+			}
+		})
+	}
+	else if (request.object.get('status') === 'active') {
+
+	}
+
 	response.success();
 });
