6af71355f09f78b10cff1cdb97606309bea6155f
Hawken Rives <hawkrives@gmail.com>
2014-12-17 02:35:30 -0600

    Show orders under the map view

--- app/components/game-orders.js
+++ app/components/game-orders.js
@@ -1,13 +1,16 @@
 import * as React from 'react'
+import * as _ from 'lodash'
 
 import gameActions from '../actions/gameActions'
 
 let Orders = React.createClass({
 	submitOrders () {
-		gameActions.submitOrders()
+		gameActions.submitOrders(this.props.gameId, this.props.pendingOrders)
 	},
 	render() {
-		let orderDialogue = React.createElement('div', {id: 'order-text'}, 'Order Dialogue Here')
+		let orderDialogue = React.createElement('div', {id: 'order-text'},
+			'Order Dialogue Here',
+			_.map(this.props.pendingOrders, (order) => React.createElement('li', {key: order.at}, JSON.stringify(order, null, 2))))
 		let submitOrders = React.createElement('button', {className: 'order-button', onClick: this.submitOrders}, 'Submit Orders')
 
 		return React.createElement('div', {id: 'game-orders'}, orderDialogue, submitOrders)
